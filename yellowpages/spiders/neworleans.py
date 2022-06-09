b2cefb1beb7084b970cd38fd048d1c2f225a6fa6
Matthew Fritscher <mfritscher555@yahoo.com>
2022-06-09 13:39:00 -0500

    update

--- yellowpages/spiders/neworleans.py
+++ yellowpages/spiders/neworleans.py
@@ -12,8 +12,9 @@ class neworleansSpider(scrapy.Spider):
     'https://www.yellowpages.com/search?search_terms=restaurants&geo_location_terms=New%20Orleans%2C%20LA&page=2']
 
     def parse(self, response):
-        file_name = 'nolaoutputfile33.csv'
-        restaurant_info = response.css('div#main-content')
+        file_name = 'nolaoutputfile35.csv'
+        #restaurant_info = response.css('div#main-content')
+        restaurants = response.css('div.result')
 
 
 
@@ -23,17 +24,22 @@ class neworleansSpider(scrapy.Spider):
         else:
             out_file = open(file_name, 'w')
             csv_writer = csv.writer(out_file)
-            csv_writer.writerow( ['name', 'phone_number', 'address', 'city_zip'])
-            #csv_writer.writerow( ['name'] )
+            #csv_writer.writerow( ['name', 'phone_number', 'address', 'city_zip'])
+            csv_writer.writerow( ['name'] )
 
-        for restaurant in restaurant_info:
-            #name = restaurant_info.css('span.a.business-name::text').extract()
-            name = restaurant_info.xpath('a.business-name/@span::text').extract()[0]
-            phone_number = restaurant_info.css('div.phones.phone.primary::text').extract()
-            address = restaurant_info.css('div.street-address::text').extract()
-            city_zip = restaurant_info.css('div.locality::text').extract()
-            csv_writer.writerow( [name, phone_number, address, city_zip] )
-            #csv_writer.writerow( [name] )
+    #    for restaurant in restaurant_info:
+    #        name = restaurant
+    #        phone_number = restaurant_info.css('div.phones.phone.primary::text').extract()
+    #        city_zip = restaurant_info.css('div.locality::text').extract()
+    #        csv_writer.writerow( [name, phone_number, address, city_zip] )
+    #        #csv_writer.writerow( [name] )
+    #        out_file.close()
+
+
+        for restaurant in restaurants:
+            name = restaurant.css('span::text').get()
+            print(name)
+            csv_writer.writerow( [ name ] )
             out_file.close()
 
 #//*[@img class="csrp"]/@alt="Acme Oyster House"
---
+++ yellowpages/spiders/neworleans2.py
@@ -0,0 +1,33 @@
+import scrapy
+import csv
+import os.path
+from lxml import html
+from lxml import etree
+import requests
+
+class neworleansSpider(scrapy.Spider):
+    name = "neworleans2"
+    start_urls = ['https://www.yellowpages.com/search?search_terms=restaurants&geo_location_terms=New%20Orleans%2C%20LA']
+    #start = 0
+    #rest_ids = []
+
+    def parse(self, response):
+        file_name = 'nolaoutputfile37.csv'
+        #restaurant_info = response.css('div#main-content')
+        restaurants = response.css('div.result')
+
+        out_file = open(file_name, 'w')
+        csv_writer = csv.writer(out_file)
+        csv_writer.writerow( ['name', 'phone_number', 'address', 'city_zip'])
+            #csv_writer.writerow( ['name'] )
+
+        for restaurant in restaurants:
+            name = restaurant.css('span::text').get()
+            phone_number = restaurant.css('div.phones.phone.primary::text').get()
+            address = restaurant.css('div.street-address::text').get()
+            city_zip = restaurant.css('div.locality::text').get()
+            print(name, phone_number, address, city_zip)
+            csv_writer.writerow( [ name, phone_number, address, city_zip ] )
+
+
+        out_file.close()
